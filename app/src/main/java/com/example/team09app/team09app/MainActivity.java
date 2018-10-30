66c4170abf29967539276ef352d4ddeaf15cf09a
AmyC <coo17045@byui.edu>
2018-10-29 22:05:15 -0400

    update by Amy

--- app/src/main/java/com/example/team09app/team09app/MainActivity.java
+++ app/src/main/java/com/example/team09app/team09app/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Hello from Amy.
         // This is Chris William Carrick 7/6
 
         // This is Amy Cook
