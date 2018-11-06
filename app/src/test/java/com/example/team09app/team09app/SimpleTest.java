03aa9b570001e8f87f7b1a6d26216781b1c40139
Chris Carrick-2 <car17059@byui.edu>
2018-11-05 19:18:25 -0700

    Added junit and SimpleTest class

---
+++ app/src/test/java/com/example/team09app/team09app/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.example.team09app.team09app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void additionIsCorrect() { assertEquals(4, 2 + 2); }
+}
+
