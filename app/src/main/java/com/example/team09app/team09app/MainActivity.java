cdbc19a93f5988effd19b1ea43b8edcf6558478b
Chris Carrick <car17059@byui.edu>
2018-11-06 12:48:18 -0700

    Main Menu Created and buttons added. Main Activity now loads the Main Menu Activity All buttons work and display a Log.d message when clicked.

--- app/src/main/java/com/example/team09app/team09app/MainActivity.java
+++ app/src/main/java/com/example/team09app/team09app/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.team09app.team09app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickContinueButton(View view) {
+
         Log.d("Main_Activity", "Button Clicked");
+        Intent mainMenuIntent = new Intent(this, MainMenu.class);
+        startActivity(mainMenuIntent);
     }
 }
--- app/src/main/java/com/example/team09app/team09app/MainMenu.java
+++ app/src/main/java/com/example/team09app/team09app/MainMenu.java
@@ -2,12 +2,35 @@ package com.example.team09app.team09app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
+    private static final String TAG = "Main_Menu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+    
+    public void addNewItem(View view) {
+        Log.d(TAG, "addNewItem: button clicked");
+    }
+    
+    public void browseByRoom(View view) {
+        Log.d(TAG, "browseByRoom: button clicked");
+    }
+    
+    public void browseByCategory(View view) {
+        Log.d(TAG, "browseByCategory: button clicked");
+    }
+    
+    public void viewAllItems(View view) {
+        Log.d(TAG, "viewAllItems: button clicked");
+    }
+    
+    public void export(View view) {
+        Log.d(TAG, "export: button clicked");
     }
 }
