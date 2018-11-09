fbcdb48f4858644957b8b79b0f24e31a6d3a63c9
Stubbs-Ian <stu15015@byui.edu>
2018-11-08 17:57:13 -0700

    new items

--- app/src/main/java/com/example/team09app/team09app/export.java
+++ app/src/main/java/com/example/team09app/team09app/export.java
@@ -8,6 +8,6 @@ public class export extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_export);
+        setContentView(R.layout.activity_export);
     }
 }
