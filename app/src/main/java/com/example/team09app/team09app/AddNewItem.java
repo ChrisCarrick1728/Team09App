b1d07f8cd62861664a4b4c72d1eb30afaa9d9bd5
ragid1 <ragid1@byui.edu>
2018-11-17 11:52:22 -0700

    Added the pop-ups for add new category and and new room to the add new item activity

--- app/src/main/java/com/example/team09app/team09app/AddNewItem.java
+++ app/src/main/java/com/example/team09app/team09app/AddNewItem.java
@@ -1,20 +1,139 @@
 package com.example.team09app.team09app;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 public class AddNewItem extends AppCompatActivity implements MainMenuButtonFunction {
 
     private static final String TAG = "Add_New_Item";
+    final Context context = this;
+    private EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
+
+        //Start of the code for the add new category pop-up to open up
+
+        ImageButton addNewCategoryPlus = (ImageButton)findViewById(R.id.addNewCategoryPlus_btn_id);
+
+        //categoryList = (ListView)findViewById(R.id.categoryList);
+
+        addNewCategoryPlus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.add_new_category_popup, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context,R.style.alertDialog);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editText2);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(true)
+                        .setPositiveButton("Save Category",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        Category newCategory = new Category();
+                                        newCategory.setName(String.valueOf(userInput.getText()));
+                                        //categoryList.add(newCategory);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
+        //End of add new category pop-up code
+
+        //Start of code for the add new room pop-up
+
+        ImageButton addNewRoomPlus = (ImageButton)findViewById(R.id.addNewRoomPlus_btn_id);
+
+        //roomList = (ListView)findViewById(R.id.roomList);
+
+        addNewRoomPlus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.add_new_room_popup, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context,R.style.alertDialog);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editText2);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(true)
+                        .setPositiveButton("Save Room",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        Room newRoom = new Room();
+                                        newRoom.setName(String.valueOf(userInput.getText()));
+                                        //roomList.add(newRoom);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+
+                // show it
+                alertDialog.show();
+
+
+            }
+        });
+
+        //End of code for add new room pop-up
     }
 
     @Override // MainMenuButtonFunction
